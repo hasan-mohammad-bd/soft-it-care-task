c1b208b179b17d3273d83c9dce902b397a9d8667
hasan-mohammad-bd <hasan.md.east@gmail.com>
2023-06-24 16:07:40 +0600

    first commit

---
+++ src/app/signUp/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+            this is signup SignUp
+        </div>
+    );
+};
+
+export default SignUp;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='flex justify-between items-center py-7 bg-gray-400 container mx-auto'>
+            <div>logo</div>
+            <div>others</div>
+        </nav>
+    );
+};
+
+export default Navbar;
